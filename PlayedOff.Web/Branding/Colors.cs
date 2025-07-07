472cf83e194d52f8b7f3cac4b9554fdec34cbbe7
Alex Hesketh <alexhesketh@outlook.com>
2025-07-07 20:48:57 +0100

    Investigate theming

---
+++ PlayedOff.Web/Branding/Colors.cs
@@ -0,0 +1,13 @@
+﻿namespace PlayedOff.Web.Branding
+{
+    public static class Colors
+    {
+        public static string Primary = "#5183b8";
+        public static string Secondary = "#ed937d";
+        public static string Tertiary = "#a0a98e";
+
+        public static string Background = "#f2f7fd";
+
+        public static string Text = "#030c16";
+    }
+}
---
+++ PlayedOff.Web/Branding/Theming.cs
@@ -0,0 +1,35 @@
+﻿using MudBlazor;
+
+namespace PlayedOff.Web.Branding
+{
+    public static class Theming
+    {
+        public static MudTheme Theme = new()
+        {
+            //PaletteLight = new PaletteLight
+            //{
+            //    Primary = Colors.Primary,
+            //    Secondary = Colors.Secondary,
+            //    Background = Colors.Background,
+            //    Tertiary = Colors.Tertiary,
+            //    AppbarBackground = Colors.Primary,
+            //    AppbarText = Colors.Text,
+            //    TextPrimary = Colors.Text,
+            //    TextSecondary = Colors.Text
+            //},
+
+            //LayoutProperties = new LayoutProperties
+            //{
+            //    AppbarHeight = "48px"
+            //},
+
+            //Typography = new Typography()
+            //{
+            //    Default = new DefaultTypography()
+            //    {
+            //        FontFamily = new[] { " Times New Roman" }
+            //    }
+            //}
+        };
+    }
+}
