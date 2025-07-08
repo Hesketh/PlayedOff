58dec5fcb76bb4e00b72c06be5134ae291e8a0ec
Alex Hesketh <alexhesketh@outlook.com>
2025-07-08 21:05:15 +0100

    Theming tweaks when loading

---
+++ PlayedOff.Web/Layout/MainLayout.razor.cs
@@ -0,0 +1,29 @@
+﻿using MudBlazor;
+
+namespace PlayedOff.Web.Layout
+{
+    public partial class MainLayout
+    {
+        public bool DarkMode { get; set; }
+        public bool DrawerOpen { get; set; }
+
+        private MudThemeProvider _mudThemeProvider = null!;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                DarkMode = await _mudThemeProvider.GetSystemDarkModeAsync();
+                await _mudThemeProvider.WatchSystemDarkModeAsync(OnSystemDarkModeChanged);
+                StateHasChanged();
+            }
+        }
+
+        private Task OnSystemDarkModeChanged(bool newValue)
+        {
+            DarkMode = newValue;
+            StateHasChanged();
+            return Task.CompletedTask;
+        }
+    }
+}
